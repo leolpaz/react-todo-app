246f45e5436b0894682b1175b078433779a4249c
leolpaz <leonardolpaz95@gmail.com>
2021-10-25 08:41:31 -0300

    Code along how to edit todos

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -2,6 +2,22 @@ import React from "react";
 import styles from "./TodoItem.module.css"
 
 export default class TodoItem extends React.Component {
+  state = {
+    editing: false,
+  }
+  
+  handleEditing = () => {
+    this.setState({
+      editing: true,
+    })
+  }
+
+  handleUpdatedDone = event => {
+    if (event.key === "Enter") {
+      this.setState({ editing: false })
+    }
+  }
+
   render() {
     const completedStyle = {
       fontStyle: "italic",
@@ -9,17 +25,40 @@ export default class TodoItem extends React.Component {
       opacity: 0.4,
       textDecoration: "line-through",
     }
+    
     const { completed, id, title } = this.props.todo
+
+    let viewMode = {}
+    let editMode = {}
+
+    if (this.state.editing) {
+      viewMode.display = "none"
+    } else {
+      editMode.display = "none"
+    }
+
     return (
       <li className={styles.item}>
+        <div style={viewMode} onDoubleClick={this.handleEditing}>
+          <input
+            type="checkbox"
+            className={styles.checkbox}
+            checked={completed}
+            onChange={() => this.props.handleChangeProps(id)}
+          />
+          <button onClick={() => this.props.deleteTodoProps(id)}>Delete</button>
+          <span style={completed ? completedStyle : null}>{title}</span>
+        </div>
         <input
-          type="checkbox"
-          className={styles.checkbox}
-          checked={completed}
-          onChange={() => this.props.handleChangeProps(id)}
+          type="text"
+          style={editMode}
+          className={styles.textInput}
+          value={title}
+          onChange={e => {
+            this.props.setUpdate(e.target.value, id)
+          }}
+          onKeyDown={this.handleUpdatedDone}
         />
-        <button onClick={() => this.props.deleteTodoProps(id)}>Delete</button>
-        <span style={completed ? completedStyle : null}>{title}</span>
       </li>
     )
   }
